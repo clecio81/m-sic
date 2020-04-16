a7b3ad099e7221f5265c355cae796aa2bcf5dab6
clecio81 <55952484+clecio81@users.noreply.github.com>
2020-04-16 14:49:49 -0300

    Delete deleteall.js

--- commands/deleteall.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-exports.run = (client, message, arg) => {
-  if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("você não tem permissão para utilizar este comando");
-  let gg = message.content.split(" ").slice(1).join(" ")
-  if(!gg)  return message.channel.send('esta função ira deletar todos os canais do seu servidor, você deseja continuar? se sim digite c!deleteall sim')
-message.guild.channels.forEach(channel => channel.delete())
-  
-}
