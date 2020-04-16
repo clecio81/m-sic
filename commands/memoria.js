e5581e62bd14084128ff272b9a9a17f9723a05a4
clecio81 <55952484+clecio81@users.noreply.github.com>
2020-04-16 16:29:55 -0300

    Update memoria.js

--- commands/memoria.js
+++ commands/memoria.js
@@ -1,3 +1,10 @@
 exports.run = (client, message, args) => {
     message.reply(`**Memoria usada:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb :timer:`);
 }
+module.exports.config = {
+    name: "memoria",
+    description: "Pulls the serverinfo of the guild!",
+    usage: "!serverinfo",
+    accessableby: "Members",
+    aliases: ["memory","memorybot"]
+}
